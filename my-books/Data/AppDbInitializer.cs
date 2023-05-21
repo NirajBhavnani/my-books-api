7fd50555c9cc0d523282d22970f6ca4023679de1
nbhavna <niraj.bhavnani@morningstar.com>
2023-05-21 22:39:58 +0530

    Added DB context and migration with seed data

---
+++ my-books/Data/AppDbInitializer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using my_books.Data.Models;
+using System;
+using System.Linq;
+
+namespace my_books.Data
+{
+    public class AppDbInitializer
+    {
+        //this method is used to add data to db, if database is empty
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using(var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                //if we don't have any books in the db
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new Book()
+                    {
+                        Title = "1st Book Title",
+                        Description = "1st Book Description",
+                        IsRead = true,
+                        DateRead = DateTime.Now.AddDays(-10),
+                        Rate = 4,
+                        Genre = "Biography",
+                        Author = "First Author",
+                        CoverUrl = "https....",
+                        DateAdded = DateTime.Now
+                    },
+                    new Book()
+                    {
+                        Title = "2nd Book Title",
+                        Description = "2nd Book Description",
+                        IsRead = false,
+                        Genre = "Biography",
+                        Author = "First Author",
+                        CoverUrl = "https....",
+                        DateAdded = DateTime.Now
+                    }
+                    );
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
