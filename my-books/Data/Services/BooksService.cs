7e7ab979ec676e487c6527e9d400bcdb3c791f23
NirajBhavnani <neerajbhavnani@gmail.com>
2023-05-21 23:52:15 +0530

    Books 1st post controller added with service and view model

---
+++ my-books/Data/Services/BooksService.cs
@@ -0,0 +1,33 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+using System;
+
+namespace my_books.Data.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+        //passing view model to maintain abstraction: e.g id, date added are not provided by the user
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.DateRead,
+                Rate = book.Rate,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+    }
+}
